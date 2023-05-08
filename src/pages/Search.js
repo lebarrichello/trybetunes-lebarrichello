d3aa489ef11b019fb1e8365f5eac6dc5c5e1e3e2
lebarrichello <leticia.barrichello@gmail.com>
2023-05-08 19:14:25 -0300

    requisito 5 - Criando Formulario para pesquisar Artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      isButtonDisabled: true,
+      loading: true,
+
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
+
+  handleSearch(event) {
+    event.preventDefault();
+    this.setState({
+      artistName: '',
+      loading: true,
+    }, async () => {
+    });
+  }
+
+  validateInputSearch({ target }) {
+    const minLength = 2;
+    this.setState({
+      artistName: target.value,
+      isButtonDisabled: target.value.length < minLength,
+    });
+  }
+
   render() {
+    const { artistName, isButtonDisabled, loading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div className="container">
+          {loading ? <Loading /> : (
+            <div className="searchContainer">
+              <form>
+                <input
+                  type="text"
+                  onChange={ (event) => this.validateInputSearch(event) }
+                  value={ artistName }
+                  className="searchArtistInput"
+                  data-testid="search-artist-input"
+                  placeholder="Nome do artista"
+                />
+                <button
+                  type="submit"
+                  disabled={ isButtonDisabled }
+                  data-testid="search-artist-button"
+                  onClick={ (event) => this.handleSearch(event) }
+                >
+                  Pesquisar
+                </button>
+              </form>
+
+            </div>
+          )}
+        </div>
       </div>
     );
   }
